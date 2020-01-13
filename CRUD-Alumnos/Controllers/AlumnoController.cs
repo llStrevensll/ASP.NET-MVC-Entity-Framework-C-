3fdb39181928cc25467f0569199df6f2d1ba1c98
Steven <anguelr12@hotmail.com>
2020-01-13 18:41:45 -0500

    Modelo, Controlador

---
+++ CRUD-Alumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,36 @@
+﻿using CRUD_Alumnos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_Alumnos.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index(){
+
+            //Conexion BD desde modelo
+            AlumnosContext db = new AlumnosContext();
+
+            //Listar - a=> Expresión lambda
+            //Uso de LinQ
+            //List<Alumnos> listaAlumnos = db.Alumnos.Where(a => a.Edad > 18).ToList();
+
+            return View(db.Alumnos.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Agregar() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(Alumnos alumnos) {
+            return View();
+        }
+
+    }
+}
